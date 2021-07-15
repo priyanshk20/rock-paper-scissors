16d50c447df19adf6e77c84e58861eaeafe49ca2
priyanshk20 <khandelwalpriyansh85@gmail.com>
2021-07-15 13:39:39 +0530

    formatted document with prettier

--- app.js
+++ app.js
@@ -8,86 +8,86 @@ const rock = document.getElementById("r");
 const paper = document.getElementById("p");
 const scissors = document.getElementById("s");
 
-
 const computerChoice = () => {
-    const choices = ["r","p","s"];
-    const random = Math.floor(Math.random() * 3);
-    return choices[random];
-}
+  const choices = ["r", "p", "s"];
+  const random = Math.floor(Math.random() * 3);
+  return choices[random];
+};
 
 const convert = (letter) => {
-    if(letter === "r") return "Rock";
-    if(letter === "p") return "Paper";
-    return "Scissors";
-}
+  if (letter === "r") return "Rock";
+  if (letter === "p") return "Paper";
+  return "Scissors";
+};
 
+const win = (userChoice, compChoice) => {
+  userScore++;
+  userElem.innerHTML = userScore;
+  compElem.innerHTML = computerScore;
+  result.innerHTML = `User's ${convert(userChoice)} beats Computer's ${convert(
+    compChoice
+  )}. You win!😁`;
+  document.getElementById(userChoice).classList.add("winner");
+  setTimeout(() => {
+    document.getElementById(userChoice).classList.remove("winner");
+  }, 1000);
+};
 
-const win = (userChoice,compChoice) => {
-    userScore++;
-    userElem.innerHTML = userScore;
-    compElem.innerHTML = computerScore;
-    result.innerHTML = `User's ${convert(userChoice)} beats Computer's ${convert(compChoice)}. You win!😁`;
-    document.getElementById(userChoice).classList.add('winner');
-    setTimeout(() => {
-        document.getElementById(userChoice).classList.remove('winner')
-    }, 1000);
-}
+const lose = (userChoice, compChoice) => {
+  computerScore++;
+  compElem.innerHTML = computerScore;
+  userElem.innerHTML = userScore;
+  result.innerHTML = `Computer's ${convert(compChoice)} beats User's ${convert(
+    userChoice
+  )}. You lose!😢`;
+  document.getElementById(userChoice).classList.add("loser");
+  setTimeout(() => {
+    document.getElementById(userChoice).classList.remove("loser");
+  }, 1000);
+};
 
-const lose = (userChoice,compChoice) => {
-    computerScore++
-    compElem.innerHTML = computerScore;
-    userElem.innerHTML =userScore;
-    result.innerHTML = `Computer's ${convert(compChoice)} beats User's ${convert(userChoice)}. You lose!😢`;
-    document.getElementById(userChoice).classList.add('loser');
-    setTimeout(() => {
-        document.getElementById(userChoice).classList.remove('loser')
-    }, 1000);
-}
-
-const draw = (userChoice,compChoice) => {
-    result.innerHTML = `Computer chose ${convert(userChoice)} too. Its a DRAW!😒`;
-    document.getElementById(userChoice).classList.add("tie");
-    setTimeout(() => {
-        document.getElementById(userChoice).classList.remove('tie')
-    }, 1000);
-}
+const draw = (userChoice, compChoice) => {
+  result.innerHTML = `Computer chose ${convert(userChoice)} too. Its a DRAW!😒`;
+  document.getElementById(userChoice).classList.add("tie");
+  setTimeout(() => {
+    document.getElementById(userChoice).classList.remove("tie");
+  }, 1000);
+};
 
 const game = (userChoice) => {
-    const compChoice = computerChoice();
+  const compChoice = computerChoice();
 
-    switch (userChoice + compChoice) {
-        case "rs":
-        case "pr":
-        case "sp":
-            win(userChoice,compChoice);  
-            break;
-        case "rp":
-        case "ps":
-        case "sr":
-            lose(userChoice,compChoice);
-            break;
-        case "rr":
-        case "ss":
-        case "pp":
-            draw(userChoice,compChoice);
-            break;
-    }
-}
+  switch (userChoice + compChoice) {
+    case "rs":
+    case "pr":
+    case "sp":
+      win(userChoice, compChoice);
+      break;
+    case "rp":
+    case "ps":
+    case "sr":
+      lose(userChoice, compChoice);
+      break;
+    case "rr":
+    case "ss":
+    case "pp":
+      draw(userChoice, compChoice);
+      break;
+  }
+};
 
 const main = () => {
+  rock.addEventListener("click", () => {
+    game("r");
+  });
 
-    rock.addEventListener("click", () =>{
-       game("r");
-    });
-    
-    paper.addEventListener("click", () =>{
-        game("p");
-    });
-    
-    scissors.addEventListener("click", () =>{
-        game("s");
-    });
+  paper.addEventListener("click", () => {
+    game("p");
+  });
 
+  scissors.addEventListener("click", () => {
+    game("s");
+  });
 };
 
 main();
